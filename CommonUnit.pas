88f99f36ccb3df7457f8ed3ad02f15c871253647
liesix66 <liesix66@gmail.com>
2014-07-25 10:27:42 +0800

    # 練習 一題物件導向設計

--- CommonUnit.pas
+++ CommonUnit.pas
@@ -4,10 +4,8 @@ interface
 type
   TCommon = class( Tobject )
 
-  function getStringCount( memostring : String):integer;
-  function getchkString(_Str : String):Boolean;
- //procedure chkString( _Str : String , _text : String);
-
+  function CalculateFibonacci( _firstNum , _secNum : int64) : int64 ;
+  function Calculategoldenratio( _firstNum , _secNum : int64) : real ;
   end;
 
 Var
@@ -15,44 +13,14 @@ Var
 
 implementation
 
-function TCommon.getchkString(_Str: string) : Boolean ;
-Var ChkVaule : Integer ;
-Var _Text : String ;
+function TCommon.CalculateFibonacci( _firstNum , _secNum : int64) : int64 ;
 begin
-  Result := False ;
-
-  ChkVaule := pos( _Str, _Text);
-  if ChkVaule > 0 then
-  begin
-    Result := true ;
-  end else
-  begin
-    Result := False ;
-  end;
+  result := _firstNum + _secNum ;
 end;
 
-function TCommon.getStringCount(memostring: string):integer;
-Var _SearchString : String ;
-Var _stringVaule : Integer ;
-Var _CountString : Integer;
+function TCommon.Calculategoldenratio( _firstNum , _secNum : int64) : real ;
 begin
-  result := 0 ;
-  _stringVaule := 0 ;
-
-  _CountString := 0 ;
-
-  if _CountString =  0 then
-  begin
-    _stringVaule := pos( _searchString, memostring);
-  end;
-
-
-  if _stringVaule > 0 then
-  begin
-    Inc( _CountString);
-    Result := _CountString ;
-  end;
-
+  result := _secNum / _firstNum ;
 end;
 
 
